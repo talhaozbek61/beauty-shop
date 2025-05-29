295574103c2809e8170f6af543214a385e3e0289
Talha <64659821+talhaozbek61@users.noreply.github.com>
2025-05-29 17:12:42 +0300

    chore: decoupled frontend from backend
    - Frontend and backend will be handled separately for now

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db-connect.js";
 import productsRouter from "./router/products.js";
-import path from "path";
 
 dotenv.config();
 const PORT = process.env.PORT;
@@ -10,18 +9,9 @@ const MONGO_URI = process.env.MONGO_URI;
 
 const app = express();
 
-const __dirname = path.resolve();
-
 app.use(express.json()); // allows us to accept JSON data in the req.body
 
 app.use("/api/products", productsRouter);
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-}
 
 app.listen(PORT, () => {
   connectDB(MONGO_URI);
